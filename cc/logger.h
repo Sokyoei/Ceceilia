111fc4714c80e8483d2c8129aef73e8c482b6e80
Sokyoei <89757286+Sokyoei@users.noreply.github.com>
2024-01-12 15:35:44 +0800

    fix cmake simd error

---
+++ cc/logger.h
@@ -0,0 +1,25 @@
+/**
+ * @file logger.h
+ * @date 2023/12/28
+ * @author Sokyoei
+ * @details
+ * C log
+ */
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void log() {}
+
+void add_handler() {}
+
+#define LOG_DEBUG()
+#define LOG_INFO()
+#define LOG_WARN()
+#define LOG_ERROR()
+#define LOG_FATAL()
+
+#endif  // !LOGGER_H
---
+++ cc/logger.hpp
@@ -0,0 +1,7 @@
+/**
+ * @file logger.hpp
+ * @date 2023/12/28
+ * @author Sokyoei
+ *
+ *
+ */
