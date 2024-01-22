8bc74a715c76ccb2a5bffaa5767a48a4cfcafbb4
Sokyoei <89757286+Sokyoei@users.noreply.github.com>
2024-01-22 15:04:13 +0800

    add client/server contants.h...

---
+++ include/Contants.h
@@ -0,0 +1,12 @@
+#ifndef CONTANTS_H
+#define CONTANTS_H
+
+// Project infomation
+#define AHTHOR "Sokyoei"
+
+#define VERSION_MAJOR "0"
+#define VERSION_MINOR "0"
+#define VERSION_PATCH "1"
+#define VERSION VERSION_MAJOR "." VERSION_MINOR "." VERSION_PATCH
+
+#endif  // !CONTANTS_H
