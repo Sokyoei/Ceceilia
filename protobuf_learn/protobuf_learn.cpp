c02c48738a47393da28fcbf606d28306d6c96a3d
Sokyoei <89757286+Sokyoei@users.noreply.github.com>
2024-11-01 15:08:12 +0800

    fix protobuf_learn

--- protobuf_learn/protobuf_learn.cpp
+++ protobuf_learn/protobuf_learn.cpp
@@ -1,10 +1,22 @@
 #include <fstream>
 #include <iostream>
+#include <string>
+
+#include <fmt/core.h>
 
 #include "protobuf_v2_learn.pb.h"
 
 int main(int argc, char const* argv[]) {
     GOOGLE_PROTOBUF_VERIFY_VERSION;
 
+    Ahri::Person person;
+    person.set_id(1);
+    person.set_name("Ahri");
+
+    std::string str;
+    person.SerializeToString(&str);
+    fmt::println("{}", str);
+
+    google::protobuf::ShutdownProtobufLibrary();
     return 0;
 }
