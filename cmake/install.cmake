0611b9760c679d25f5a2821f0a75092fc52d3754
Sokyoei <89757286+Sokyoei@users.noreply.github.com>
2025-04-29 16:07:46 +0800

    update

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -1,10 +1,29 @@
 set(CMAKE_INSTALL_PREFIX "${CMAKE_BINARY_DIR}/install" CACHE PATH "Installation Directory")
 
-macro(install_ceceilia_exe exe)
-    install(TARGETS ${exe} RUNTIME DESTINATION bin)
-endmacro(install_ceceilia_exe)
+macro(install_exe exe)
+    # install(
+    #     TARGETS ${exe}
+    #     RUNTIME_DEPENDENCIES
+    #     PRE_EXCLUDE_REGEXES "api-ms-.*" "ext-ms-.*"
+    #     POST_EXCLUDE_REGEXES ".*system32/.*\\.dll"
+    #     RUNTIME DESTINATION bin
+    # )
 
-macro(install_ceceilia_lib lib)
+    install(
+        TARGETS ${exe}
+        RUNTIME_DEPENDENCY_SET runtime_deps
+        RUNTIME
+        DESTINATION bin
+    )
+    # install(
+    #     RUNTIME_DEPENDENCY_SET runtime_deps
+    #     PRE_EXCLUDE_REGEXES "api-ms-" "ext-ms-"
+    #     POST_EXCLUDE_REGEXES ".*system32/.*\\.dll"
+    #     RUNTIME DESTINATION bin
+    # )
+endmacro(install_exe)
+
+macro(install_lib lib)
     install(
         TARGETS ${lib}
         EXPORT ${PROJECT_NAME}Targets
@@ -13,4 +32,4 @@ macro(install_ceceilia_lib lib)
         RUNTIME DESTINATION bin
         INCLUDES DESTINATION include
     )
-endmacro(install_ceceilia_lib)
+endmacro(install_lib)
