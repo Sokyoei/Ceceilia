d71f6c2935b377a583b64b8777e045c2df2b92d8
Sokyoei <89757286+Sokyoei@users.noreply.github.com>
2025-07-29 09:43:49 +0800

    merge cuda_utils.cuh to Ahri.cuh

--- include/Ahri.cuh
+++ include/Ahri.cuh
@@ -2,7 +2,21 @@
 #ifndef AHRI_CUH
 #define AHRI_CUH
 
+#include <stdio.h>
+
 #include "Ahri.hpp"
-#include "cuda_utils.cuh"
+
+#include <cuda_runtime_api.h>
+
+namespace Ahri::CUDA {
+#define CUDA_CHECK(Callable)                                                                                  \
+    do {                                                                                                      \
+        cudaError_t error = Callable;                                                                         \
+        if (error != cudaSuccess) {                                                                           \
+            fprintf(stderr, "[%s %s][%s:%d][%s][CUDA ERROR: %s, %s]", __DATE__, __TIME__, __FILE__, __LINE__, \
+                    __func__, cudaGetErrorName(error), cudaGetErrorString(error));                            \
+        }                                                                                                     \
+    } while (0)
+}  // namespace Ahri::CUDA
 
 #endif  // !AHRI_CUH
