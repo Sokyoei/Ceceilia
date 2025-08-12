3cb900a9ed3f8812da658e452e8c7d645d916f69
Sokyoei <89757286+Sokyoei@users.noreply.github.com>
2025-08-12 11:27:27 +0800

    update loadlibrary.hpp

--- include/Ahri/Ceceilia/utils/loadlibrary.hpp
+++ include/Ahri/Ceceilia/utils/loadlibrary.hpp
@@ -2,6 +2,7 @@
 #ifndef AHRI_CECEILIA_UTILS_LOADLIBRARY_HPP
 #define AHRI_CECEILIA_UTILS_LOADLIBRARY_HPP
 
+#include <filesystem>
 #include <string>
 
 #ifdef _WIN32
@@ -12,6 +13,8 @@
 
 #include <fmt/core.h>
 
+#include "Ahri/Ceceilia/utils/logger_utils.hpp"
+
 namespace Ahri {
 class AhriLoadLibrary  // rename for Windows LoadLibrary macro
 {
@@ -19,25 +22,38 @@ public:
     AhriLoadLibrary(std::string library_name) : _library_name(library_name) {
 #ifdef _WIN32
         _platform_library_name = fmt::format("{}.dll", _library_name);
+        handle = LoadLibraryEx(std::filesystem::path(_platform_library_name).wstring().c_str(), nullptr,
+                               LOAD_WITH_ALTERED_SEARCH_PATH);
+        if (!handle) {
+            DWORD error = GetLastError();
+            AHRI_LOGGER_ERROR("Failed to load plugin library. Error: {}", error);
+            return 1;
+        } else {
+            AHRI_LOGGER_INFO("Load {} success", _platform_library_name);
+        }
 #elif defined(__linux__)
         _platform_library_name = fmt::format("lib{}.so", _library_name);
-        _handle = dlopen(_platform_library_name.c_str(), RTLD_LAZY);
+        handle = dlopen(_platform_library_name.c_str(), RTLD_LAZY);
 #endif
     }
 
     ~AhriLoadLibrary() {
 #ifdef _WIN32
+        FreeLibrary(handle);
 #elif defined(__linux__)
-        dlclose(_handle);
+        dlclose(handle);
 #endif
     }
 
 private:
     std::string _library_name;
     std::string _platform_library_name;
+
+public:
 #ifdef _WIN32
+    HMODULE handle;
 #elif defined(__linux__)
-    void* _handle;
+    void* handle;
 #endif
 };
 }  // namespace Ahri
